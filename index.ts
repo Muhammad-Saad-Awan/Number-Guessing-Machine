70695191d05207da096f4f0d7ca77efb229977da
Muhammad Saad <148006898+ssaadd-bot@users.noreply.github.com>
2024-03-23 00:48:53 +0500

    Number Guessing Machine

---
+++ index.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+const randomNumber=Math.floor(Math.random()*6+1);
+
+const answers=await inquirer.prompt([
+    {
+    name:"userGuessedNumber",
+    type:"number",
+    message:" Please Guess a Number between 1-6:",
+   },
+])
+console.log(answers);
+if(answers.userGuessedNumber===randomNumber){
+    console.log(" Congragulation! You guessed right number")
+}
+else{
+    console.log("Oops! You guessed wrong number")
+}
