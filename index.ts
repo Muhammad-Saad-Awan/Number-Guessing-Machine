8ba7f9bce16897f0a77bae8e0d21f6c914bef616
Muhammad Saad <148006898+ssaadd-bot@users.noreply.github.com>
2024-03-23 12:24:59 +0500

    Number Guessing Game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber=Math.floor(Math.random()*6+1);
+
+const answers= await inquirer.prompt([
+    {
+    name:"userGuessedNumber",
+    type:"number",
+    message:" Please Guess a Number between 1-6:",
+   },
+])
+console.log(answers);
+if(answers.userGuessedNumber===randomNumber){
+    console.log(" Congragulation! You guessed right number")
+}
+else{
+    console.log("Oops! You guessed wrong number")
+}
